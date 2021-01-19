73975083becb719d1ee7bc00af09938a1cd24d2c
shreya_s <shreya.suroliya@createbytes.com>
2021-01-19 15:18:16 +0530

    input added

--- src/Components/Todolist/List.js
+++ src/Components/Todolist/List.js
@@ -3,21 +3,78 @@ import styled from 'styled-components/macro'
 
 const Component = styled.div`
     padding: 1rem;
-    border-bottom: 1px solid #e2e8f0;
+    //border-bottom: 1px solid #e2e8f0;
 `
-const Item = styled.div``
-const Complete = styled.button``
-const label = styled.p``
-const Delete = styled.button``
-const List = ({ items }) => (
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+
+    &:not(:last-of-type) {
+        margin-bottom: 1.5rem;
+    }
+`
+const buttonStyles = `
+    background-color: transparent;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0rem;
+    font-size: 1.1rem;
+    border:0;
+
+    &:focus {
+        outline: 0;
+    }
+
+    &:hover {
+        cursor: pointer;
+        filter: brightness(105%);
+    }
+
+
+
+`
+const Complete = styled.button`
+    ${buttonStyles};
+    border: ${props => props.completed ? 0 : '1px solid #e2e8f0'};
+    border: ${props => props.completed ? 0 : '1px solid #e2e8f0'};
+
+
+`
+const Label = styled.p`
+    margin: 0 0 0 0.5rem;
+    flex: 1;
+ 
+    text-decoration: ${(props) => (props.completed ? 'line-through' : 'none' )};
+    color: ${(props) => (props.completed ? '#808080' : 'black' )};
+    
+    &:hover {
+        cursor: pointer;
+        color: #4a5568;
+    } 
+`
+const Delete = styled.button`
+    ${buttonStyles};
+`
+const NoItems = styled.p`
+ margin: 0;
+ text-align: center;
+ color: red;
+`
+
+const List = ({ items, onComplete, onDelete }) => (
     <Component>
-        {items.map((item) => (
-            <Item key={item.id}>
-                <Complete>completed</Complete>
-                <label>{item.label}</label>
-                <Delete>X</Delete>
+        {items.map(({id, completed, label}) => (
+            <Item key={id}>
+                <Complete completed={completed} onClick={onComplete(id)} >{completed && '✅'}</Complete>
+                <Label completed={completed}>{label}</Label>
+                <Delete onClick={onDelete(id)}> ❌ </Delete>
             </Item>
         ))}
+        {items.length === 0 && <NoItems>Oops... Your todo list is empty</NoItems>}
     </Component>
 )
 export default List
--- src/Components/Todolist/Todolist.js
+++ src/Components/Todolist/Todolist.js
@@ -37,14 +37,33 @@ class Todolist extends Component {
     handleModeChange = mode => () => {
         this.setState({ mode })
     }
+    handleComplete = id => () => {
+        const newItems = [ ...this.state.items]
+
+        const filteredItem = newItems.filter((item) => item.id === id)[0]
+
+        filteredItem.completed = !filteredItem.completed
+
+        this.setState({ item: newItems })
+        //console.log(filteredItem)
+    }
+
+    handleDelete = id => () => {
+        const filteredItems = this.state.items.filter(item => item.id !== id)
+
+        this.setState({ items: filteredItems })
+    }
+    handleTaskAdd = (value) => {
+        console.log(value)
+    }
 
     render () {
         const { mode, items } = this.state
         return (
            <Wrapper>
                <Filter mode={mode} onModeChange={this.handleModeChange} />
-               <Input/>
-               <List items={items}/>
+               <Input onTaskAdd={this.handleTaskAdd} />
+               <List items={items} onComplete={this.handleComplete} onDelete={this.handleDelete}/>
            </Wrapper>
            
 
